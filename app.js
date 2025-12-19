85d159384ce10ea58c878ec13f131458f0f9f38b
Maksim <lukashmaxim1@gmail.com>
2025-12-19 15:40:02 +0200

    Little Changes

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const select = document.querySelector(".select_item-menu");
     const selectContainer = document.querySelector(".select_item-menu__container");
-    const selectList = document.querySelector(".select_item-menu__list")
+    const selectList = document.querySelector(".select_item-menu__list");
+    const selectLinks = document.querySelectorAll(".select_item-menu__link a");
     const selectOpener = document.querySelector(".select_item-menu__opener");
-    if(select && selectContainer && selectList && selectOpener){
+    if(select && selectContainer && selectList && selectOpener && selectLinks.length !== 0){
         selectOpener.addEventListener("click", () => {
             select.classList.toggle("_active");
             if(select.classList.contains("_active")){
@@ -33,19 +34,58 @@ document.addEventListener("DOMContentLoaded", () => {
                 selectContainer.style.height = "0px";
             }
         })
+        selectLinks[0].addEventListener("focus", () => {
+            select.classList.add("_active");
+            selectContainer.style.height = selectList.offsetHeight + "px";
+        });
+        selectLinks[selectLinks.length - 1].addEventListener("blur", () => {
+            select.classList.remove("_active");
+            selectContainer.style.height = "0px";
+        });
     }
 
     const opener = document.querySelector(".menu__burger");
     const closer = document.querySelector(".header__closer");
     const header = document.querySelector(".header");
     if(header && opener && closer){
-        opener.addEventListener("click", () => {
+        const allActiveEls = document.querySelectorAll("a, button, input");
+        const menuActiveEls = document.querySelectorAll(".menu a, .menu button:not(.burger), .header__closer");
+        const firstMenuEl = menuActiveEls[0];
+        const lastMenuEl = menuActiveEls[menuActiveEls.length - 1];
+
+        const outherActiveEls = [...allActiveEls].filter(el => ![...menuActiveEls].includes(el));
+
+        const afterHeaderOpener = outherActiveEls[[...outherActiveEls].findIndex( el => el === opener) + 1];
+
+        if(window.innerWidth <= 768){
+            menuActiveEls.forEach(el => el.tabIndex = -1);
+        }
+
+        const openHeader = (need = false) =>{
             header.classList.add("_active");
             document.body.classList.add("_locked");
-        });
-        closer.addEventListener("click", () => {
+
+            outherActiveEls.forEach(el => el.tabIndex = -1);
+            menuActiveEls.forEach(el => el.tabIndex = 0);
+
+            if(need && firstMenuEl) firstMenuEl.focus();
+        }
+
+        const closeHeader = (need = false) => {
             header.classList.remove("_active");
             document.body.classList.remove("_locked");
-        });
+
+            outherActiveEls.forEach(el => el.tabIndex = 0);
+            menuActiveEls.forEach(el => el.tabIndex = -1);
+
+            if(need && afterHeaderOpener) afterHeaderOpener.focus();
+        }
+
+        opener.addEventListener("click", openHeader);
+        opener.addEventListener("blur", () => openHeader(true));
+        closer.addEventListener("click", closeHeader);
+        if(lastMenuEl){
+            lastMenuEl.addEventListener("blur", () => closeHeader(true));
+        }
     }
 })
